1ab67f4e1a41c7c828e800ac038c7650c71466f3
Masatoshi Katabuchi <mattocci27@gmail.com>
2021-08-28 09:11:47 +0000

    test

--- assets/NJC-stat/render.R
+++
@@ -1,8 +0,0 @@
-
-#remotes::install_github("jhelvy/xaringanBuilder")
-#remotes::install_github('rstudio/chromote')
-
-xaringanBuilder::build_pdf("data_handling.Rmd",
-                           complex_slides = TRUE,
-                           partial_slides = TRUE
-)
